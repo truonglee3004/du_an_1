2996e6e452878227d6569ad2797c839261ca921e
truonglee3004 <58621121+truonglee3004@users.noreply.github.com>
2023-03-22 03:28:37 +0700

    ui/ux update

---
+++ view/body.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+
+  <body class="bg-gray-300 max-w-7xl mx-auto">
+    <section class="container mx-auto mt-20 items-center">
+      <div class="font-semibold text-3xl text-center uppercase">
+        <h3>Special Deals</h3>
+      </div>
+      <div class="flex col-span-4 mt-12 justify-between">
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center flex-col">
+        <button
+          class="mt-20 bg-white w-32 h-10 text-2xl text-center font-semibold"
+        >
+          <a href="">More</a>
+        </button>
+      </div>
+    </section>
+    <section class="container mx-auto mt-20 items-center">
+      <div class="font-semibold text-3xl text-center uppercase">
+        <h3>nike</h3>
+      </div>
+      <div class="flex col-span-4 mt-12 justify-between">
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Nike</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Nike</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Nike</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Nike</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center flex-col">
+        <button
+          class="mt-20 bg-white w-32 h-10 text-2xl text-center font-semibold"
+        >
+          <a href="">More</a>
+        </button>
+      </div>
+    </section>
+    <section class="container mx-auto mt-20 items-center">
+      <div class="font-semibold text-3xl text-center uppercase">
+        <h3>adidas</h3>
+      </div>
+      <div class="flex col-span-4 mt-12 justify-between">
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Adidas</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center flex-col">
+        <button
+          class="mt-20 bg-white w-32 h-10 text-2xl text-center font-semibold"
+        >
+          <a href="">More</a>
+        </button>
+      </div>
+    </section>
+    <section class="container mx-auto mt-20 items-center">
+      <div class="font-semibold text-3xl text-center uppercase">
+        <h3>puma</h3>
+      </div>
+      <div class="flex col-span-4 mt-12 justify-between">
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Puma</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Puma</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Puma</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white rounded-md h-fit w-auto">
+          <div class="rounded-md">
+            <img src="../img/ezgif 2.png" alt="" />
+          </div>
+          <div class="ml-2 pb-8">
+            <div class="text-2xl mt-4 hover:text-orange-500 w-fit">
+              <a href="">Áo Thun Tập Luyện Techfit</a>
+            </div>
+            <div class="mt-2 text-gray-600 hover:text-orange-500 w-fit">
+              <a href="">Puma</a>
+            </div>
+            <div class="flex mt-3 gap-4">
+              <p>722,000đ</p>
+              <del class="text-red-500">850,000đ</del>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center flex-col">
+        <button
+          class="mt-20 bg-white w-32 h-10 text-2xl text-center font-semibold"
+        >
+          <a href="">More</a>
+        </button>
+      </div>
+    </section>
+  </body>
+</html>
